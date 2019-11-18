de3fb2503bb3d64aa164d870967091b6c0b8044c
Michael Goodson <goodson128+git@gmail.com>
2019-11-17 23:43:20 -0800

    heroku

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ require("./routes/htmlRoutes")(app);
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 // Start the server
 app.listen(PORT, function() {
