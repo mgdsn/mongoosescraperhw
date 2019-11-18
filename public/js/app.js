742aba89b879c7ffbe0532244a95aa258f42480d
Michael Goodson <goodson128+git@gmail.com>
2019-11-17 21:36:23 -0800

    progress

---
+++ public/js/app.js
@@ -0,0 +1,63 @@
+  $.when(
+    $.ajax("/scrape"),
+    )
+    .done(function(){
+      $.getJSON("/articles", function(data) {
+        for (var i = 0; i < data.length; i++) {
+          $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + 
+          "<br />" + "<a href='"+ data[i].link +"'>" + "Link" + "</a>" + "<br />" + "Summary: " 
+          +  data[i].summary + "</p>" + "<p class='notes' data-id='" + data[i]._id + "'>Notes</p>");
+        }
+      })
+    })
+    .fail(function(){
+        console.log("oops")
+    });
+
+    $(document).on("click", ".notes", function() {
+      
+      var thisId = $(this).attr("data-id");
+
+    $(".modal-footer").append('<button type="button" class="btn btn-secondary submitNote" data-id="' + thisId + '">Add Note</button>');
+      $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+      })
+
+        .then(function(data) {
+          console.log(data);
+          for (var i = 0; i < data.length; i++) {
+            $("#notes").append(data.body);
+          }
+          $("#results").modal("toggle");
+
+
+        });
+    });
+
+    $(document).on("click", ".submitNote", function() {
+      // Grab the id associated with the article from the submit button
+      var thisId = $(this).attr("data-id");
+    
+      // Run a POST request to change the note, using what's entered in the inputs
+      $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+          headlineId: thisId,
+          body: $("#noteinput").val(),
+
+        }
+      })
+        // With that done
+        .then(function(data) {
+          // Log the response
+          console.log(data);
+          // Empty the notes section
+          $("#notes").empty();
+        });
+    
+      // Also, remove the values entered in the input and textarea for note entry
+      $("#titleinput").val("");
+      $("#bodyinput").val("");
+    });
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Connect to the Mongo DB
+//mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+
+module.exports = app;
